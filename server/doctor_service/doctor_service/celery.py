de032078f1609bdb9166ea4ff0542bcc95479367
GathaiKE <gathaikariuki@gmail.com>
2025-04-15 18:35:55 +0300

    FEAT: Create doctor calender task triggered from the doctor service

--- server/doctor_service/doctor_service/celery.py
+++ server/doctor_service/doctor_service/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doctor_service.settings')
 
-app=Celery('doctor_service', broker=settings.CELERY_BROKER_URL)
+app=Celery('doctor_service')
 
 app.config_from_object("django.conf.settings", namespace="CELERY")
 

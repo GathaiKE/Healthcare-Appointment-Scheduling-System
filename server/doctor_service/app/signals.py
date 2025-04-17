57cc2cc2f431e9b7e657dfaa97d034929bb3dbcb
GathaiKE <gathaikariuki@gmail.com>
2025-04-17 20:15:13 +0300

    MODELS: Patient and medical record

--- server/doctor_service/app/signals.py
+++ server/doctor_service/app/signals.py
@@ -1,11 +1,11 @@
-# from celery import Celery
-# from django.conf import settings
-
-# app=Celery('doctor_service', broker=settings.CELERY_BROKER_URL)
-# from celery import current_app as app
 from doctor_service.celery import app
+
+
 def create_schedue(doctor_id, **kwargs):
     app.send_task('create_doctor_calendar', args=[doctor_id], kwargs={**kwargs}, queue='appointments')
+
+def delete_schedule(doctor_id):
+    app.send_task('delete_doctor_calender', args={doctor_id}, queue='appointments')
 
 def unlink_records(doctor_id):
     app.send_task("unlink_doctor", args=[doctor_id], queue='medical_records')
--- server/medical_records_service/app/models.py
+++ server/medical_records_service/app/models.py
@@ -12,6 +12,20 @@ class Test(models.Model):
     def __str__(self):
         return self.name
 
+class HIVStatus(models.IntegerChoices):
+    NEGATIVE=0, 'negative',
+    POSITIVE=1, 'positive'
+
+class BloodGroup(models.IntegerChoices):
+    A_POSITIVE=0, 'A+'
+    A_NEGATIVE=1, 'A-'
+    B_POSITIVE=2, 'B+'
+    B_NEGATIVE=3, 'B-'
+    AB_POSITIVE=4, 'AB+'
+    AB_NEGATIVE=5, 'AB-'
+    O_NEGATIVE=6, 'O-'
+    O_POSITIVE=7, 'O+'
+
 class MedicalRecord(models.Model):
     id=models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
     tests=models.ManyToManyField(Test, related_name="medical_records")
@@ -19,6 +33,16 @@ class MedicalRecord(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(blank=True, null=True)
     deleted_at=models.DateTimeField(null=True, blank=True)
+    age=models.IntegerField(null=False, blank=False)
+    height=models.IntegerField(null=False, blank=False)
+    weight=models.IntegerField(null=False, blank=False)
+    hiv_status=models.SmallIntegerField(choices=HIVStatus.choices)
+    blood_group=models.SmallIntegerField(choices=BloodGroup.choices)
+    body_temperature=models.IntegerField(null=False, blank=False)
+
+    
+    body_mass_index=models.IntegerField(null=False, blank=False)
+    blood_pressure=models.CharField(null=False, blank=False)
 
     def __str__(self):
         return f"Record {self.id}"

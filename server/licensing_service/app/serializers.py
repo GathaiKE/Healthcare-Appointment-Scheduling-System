191f6b1f5ba600d2ea5ec7a487869e20a5946468
GathaiKE <gathaikariuki@gmail.com>
2025-05-01 19:06:30 +0300

    BUG: File storage changed to local and url debug routing set

--- server/licensing_service/app/serializers.py
+++ server/licensing_service/app/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from django.utils import timezone
+from django.core.validators import FileExtensionValidator
 
 from .models import License,User
 
 class UserSerializer(serializers.ModelSerializer):
+    face_img=serializers.ImageField(allow_empty_file=False, validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])], required=False)
+    practicing_certificate=serializers.FileField(allow_empty_file=False, validators=[FileExtensionValidator(allowed_extensions=['pdf'])], required=False)
+    identity_card=serializers.FileField(allow_empty_file=False, validators=[FileExtensionValidator(allowed_extensions=['pdf'])], required=False)
+    
     class Meta:
         model=User
         fields=['id','user_id','face_img','practicing_certificate','identity_card','valid_practicing_certificate','valid_identity_card','valid_face_img','created_at','updated_at']
@@ -25,9 +30,16 @@ class LicenseSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user_data=validated_data.pop('user')
-        user=User.objects.create(**user_data)
+        if not user_data.get('user_id'):
+            raise serializers.ValidationError("User data is required.")
+        
+        face_img=user_data.get('face_img', None)
+        practicing_certificate=user_data.get('practicing_certificate', None)
+        identity_card=user_data.get('identity_card', None)
+        user=User.objects.create(user_id=user_data.get('user_id'), face_img=face_img, practicing_certificate=practicing_certificate, identity_card=identity_card)
         expiration_date=timezone.now() + timezone.timedelta(days=3650)
-        license=License.objects.create(user=user, expiration_date=expiration_date, **validated_data)
+        status=License.LicenseStatus.PENDING
+        license=License.objects.create(user=user, expiration_date=expiration_date, status=status)
         return license
     
     def update(self, instance, validated_data):
